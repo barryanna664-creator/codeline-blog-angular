b85b93141b8459f9e08223baa8c489b1812d0158
Anna Barry <barryanna664@gmail.com>
2026-09-12 00:55:33 +0100

    Ajout du modele, des donnees, du service et des composants navbar/footer

---
+++ src/app/pages/article-detail/article-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-article-detail',
+  styleUrl: './article-detail.css',
+  templateUrl: './article-detail.html',
+})
+export class ArticleDetail {}
---
+++ src/app/services/article.ts
@@ -0,0 +1,24 @@
+﻿import { Injectable } from '@angular/core';
+import { Article } from '../models/article.model';
+import { ARTICLES } from '../data/articles.data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+
+  private articles: Article[] = ARTICLES;
+
+  getArticles(): Article[] {
+    return this.articles;
+  }
+
+  getArticleById(id: number): Article | undefined {
+    return this.articles.find(article => article.id === id);
+  }
+
+  getCategories(): string[] {
+    const toutesCategories = this.articles.map(a => a.categorie);
+    return Array.from(new Set(toutesCategories));
+  }
+}
