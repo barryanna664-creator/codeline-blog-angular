e2bebcc8049c44078c43261587012dc1d02b4707
Anna Barry <barryanna664@gmail.com>
2026-09-12 13:06:05 +0100

    Ajout du composant article-card

--- src/app/components/article-card/article-card.ts
+++ src/app/components/article-card/article-card.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { Article } from '../../models/article.model';
 
 @Component({
-  imports: [],
   selector: 'app-article-card',
+  imports: [ RouterLink, DatePipe],
   styleUrl: './article-card.css',
   templateUrl: './article-card.html',
 })
-export class ArticleCard {}
+export class ArticleCard {
+  @Input() article!: Article;
+}
