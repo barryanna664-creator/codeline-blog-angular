2cf31dbe349f9227e969fd21edfa02900256e2ae
Nogaye Diouf <nogayediouf384@gmail.com>
2026-09-13 12:54:29 +0000

    feat:ajout pages About et contact

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticleCard } from '../../components/article-card/article-card';
+import { ARTICLES } from '../../data/articles.data';
+import { CommonModule } from '@angular/common';
+import { Article } from '../../models/article.model';
 
 @Component({
-  imports: [],
+  imports: [CommonModule, ArticleCard],
   selector: 'app-home',
+  standalone:true,
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home implements OnInit {
+articles: Article[] = [];
+categories: string[] = [];
+categoriesSelectionnee: string = 'Toutes';
+articlesFiltres:Article[] = [];
+
+ngOnInit(): void{
+  this.articles = ARTICLES;
+  this.categories=['Toutes', ...new 
+   Set(ARTICLES.map(a => a.categorie))];
+   this.articlesFiltres = ARTICLES;
+    
+  }
+   filtrerParCategorie(categorie:string): void {
+     this.categoriesSelectionnee = categorie;
+     if (categorie ==='Toutes'){
+      this.articlesFiltres = this.articles;
+     }else{
+      this.articlesFiltres = this.articles.filter(a => a.categorie === categorie);
+     }
+   }
+} 
+
+  
+
